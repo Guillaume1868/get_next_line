fd139694f3571da547f93d9437e3e16b0facba3a
Guillaume <Guillaume@aubert.ws>
2021-10-15 16:46:33 +0200

    Test dual fd in paralel

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 int	main(int argc, char **argv)
 {
 	int		fd;
+	int		fd2;
 	char	*line;
+	char	*line2;
 
 	line = NULL;
 	if (argc == 2)
@@ -18,6 +20,27 @@ int	main(int argc, char **argv)
 			line = get_next_line(fd);
 		}
 		close(fd);
+	}
+	if (argc == 3)
+	{
+		fd = open(argv[1], O_RDONLY);
+		fd2 = open(argv[2], O_RDONLY);
+		line = get_next_line(fd);
+		line2 = get_next_line(fd2);
+		while (line > 0)
+		{
+			printf("%s", line);
+			free(line);
+			line = get_next_line(fd);
+		}
+		while (line2 > 0)
+		{
+			printf("%s", line2);
+			free(line2);
+			line2 = get_next_line(fd2);
+		}
+		close(fd);
+		close(fd2);
 	}
 	else
 		printf("\n");
